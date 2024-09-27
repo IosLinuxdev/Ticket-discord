54c7bef83b626c34ecade4ef46db9aaf046f4f66
IosLinuxdev <police123@laposte.net>
2024-09-27 20:01:03 +0200

    Ticket-discord.dll

--- ticket-discord.py
+++ ticket-discord.py
@@ -1,25 +1,73 @@
-import nova_life_plugin
+// This C# code is intended for a web application that uses a Discord webhook for notifications.
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
 
-def on_ticket_created(ticket):
-    # Get the ticket details
-    title = ticket.title
-    creator_name = ticket.creator.name
-    creator_ip = ticket.creator.ip
-    ban_count = ticket.creator.ban_count
-    warn_count = ticket.creator.warn_count
+public class Ticket
+{
+    public string Title { get; set; }
+    public Creator Creator { get; set; }
+}
 
-    # Create a Discord embed
-    embed = {
-        "title": title,
-        "description": f"Ticket created by {creator_name} ({creator_ip})",
-        "fields": [
-            {"name": "Bans", "value": ban_count, "inline": True},
-            {"name": "Warns", "value": warn_count, "inline": True}
-        ]
+public class Creator
+{
+    public string Name { get; set; }
+    public string Ip { get; set; }
+    public int BanCount { get; set; }
+    public int WarnCount { get; set; }
+}
+
+public class EmbedField
+{
+    public string Name { get; set; }
+    public string Value { get; set; }
+    public bool Inline { get; set; }
+}
+
+public class Embed
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public List<EmbedField> Fields { get; set; }
+}
+
+public class TicketHandler
+{
+    private static readonly HttpClient httpClient = new HttpClient();
+    
+    public static async void OnTicketCreated(Ticket ticket)
+    {
+        // Get the ticket details
+        string title = ticket.Title;
+        string creatorName = ticket.Creator.Name;
+        string creatorIp = ticket.Creator.Ip;
+        int banCount = ticket.Creator.BanCount;
+        int warnCount = ticket.Creator.WarnCount;
+
+        // Create a Discord embed
+        Embed embed = new Embed
+        {
+            Title = title,
+            Description = $"Ticket created by {creatorName} ({creatorIp})",
+            Fields = new List<EmbedField>
+            {
+                new EmbedField { Name = "Bans", Value = banCount.ToString(), Inline = true },
+                new EmbedField { Name = "Warns", Value = warnCount.ToString(), Inline = true }
+            }
+        };
+
+        // Send the webhook notification
+        string webhookUrl = "https://discord.com/api/webhooks/1289254384924098561/nOAFkyYmX4KA8euULOx3Q4iTLBdci6qCOs2rzTNlOeSR_L5qnNhSPK7B-p6SP5LmzJrk";
+        var json = JsonConvert.SerializeObject(new { embeds = new[] { embed } });
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+        await httpClient.PostAsync(webhookUrl, content);
     }
-
-    # Send the webhook notification
-    webhook_url = "https://discord.com/api/webhooks/1289254384924098561/nOAFkyYmX4KA8euULOx3Q4iTLBdci6qCOs2rzTNlOeSR_L5qnNhSPK7B-p6SP5LmzJrk"
-    requests.post(webhook_url, json={"embeds": [embed]})
-
-nova_life_plugin.register_event("on_ticket_created", on_ticket_created)
+    
+    // Registering the event handler can be done in the initialization part of your application
+    public static void RegisterEvent()
+    {
+        // Assuming there is a method to register the event
+        // YourEventSystem.Register("on_ticket_created", OnTicketCreated);
+    }
+}
